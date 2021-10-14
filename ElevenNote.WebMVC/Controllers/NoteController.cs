01dacb0ecc90f36b32110e44e2ed5bfc04a2507a
leilahsmith <leilah.sdev@gmail.com>
2021-10-14 18:33:19 -0300

    Created Note Controller.

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,8 @@ namespace ElevenNote.WebMVC.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            return View();
+            var model = new NoteListItem[0];
+            return View(model);
         }
     }
 }
