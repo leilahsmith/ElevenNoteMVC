43d2f3da8fa7cb7e74779b5f8d6ad2cf219d855a
leilahsmith <leilah.sdev@gmail.com>
2021-10-14 21:22:11 -0300

    Added Star properties and view

--- ElevenNote.Data/Migrations/Configuration.cs
+++ ElevenNote.Data/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace ElevenNote.Data.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(ElevenNote.Data.ApplicationDbContext context)
